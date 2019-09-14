b16f2e9485d004bda40ef25b4e37f637abe0af1e
Jorge Navarro <jorgeebn16@gmail.com>
2019-09-13 22:06:33 -0400

    Update 09/13/19 at 10:00 pm

--- Stu_DownToInput.py
+++
@@ -1,9 +0,0 @@
-your_first_name = input("What is your first name? ")
-neighbor_first_name = input("What's your neighbor first name? ")
-
-me_coding = int(input("How many months had you been coding? "))
-neighbor_coding = int(input("How many months me neighbor has been coding? "))
-
-total_months = me_coding + neighbor_coding
-
-print(f"Together we have been coding for {total_months} months!")
